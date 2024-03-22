68fd654ecf92637dd584b4017b938eb1efe3b17b
8Dion8 <shvartser@proton.me>
2024-03-22 21:10:02 +0200

    general yt handling without command

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ config = dotenv_values(".env")
 TOKEN = config["TOKEN"]
 AUTHOR_TG_ID = config["AUTHOR_TG_ID"]
 
+YOUTUBE_LINK_REGEX = r"(.*(www\.youtube\.com\/.+|youtu\.be\/.+)|(.+))"
+
 
 
 bot = tb.TeleBot(TOKEN, parse_mode="MARKDOWN")
@@ -17,16 +19,27 @@ bot = tb.TeleBot(TOKEN, parse_mode="MARKDOWN")
 
 bot.send_message(AUTHOR_TG_ID, "Bot online.")
 
+
+@bot.message_handler(func=lambda message: True)
+def general_handler(message):
+    text = message.text
+    chat_id = message.chat.id
+
+    if re.match(YOUTUBE_LINK_REGEX, text):
+        youtube_handler(message)
+
+
+
 @bot.message_handler(commands=["y", "youtube"])
 def youtube_handler(message):
     text = message.text
     chat_id = message.chat.id
+    
 
-    content_regex = re.search(r"(\/youtube|\/y)\s(.*(www\.youtube\.com\/.+|youtu\.be\/.+)|(.+))", text)
-
+    content_regex = re.match(YOUTUBE_LINK_REGEX, text)
     if content_regex:
         bot_core_lib.handle_user_dir_creation(str(chat_id))
-        yt_link_group = content_regex.group(3)
+        yt_link_group = content_regex.group(2)
         if yt_link_group:
             yt_link = content_regex.group(2)
             #bot.send_message(chat_id, f"found yt link: `{yt_link}`")
@@ -38,12 +51,14 @@ def youtube_handler(message):
             
             bot.send_video(chat_id, InputFile(downloaded_file_path))
             return 0
-        yt_search_query = content_regex.group(4)
+        yt_search_query = content_regex.group(3)
         if yt_search_query:
             bot.send_message(chat_id, f"found search query: `{yt_search_query}`")
             return 0
     else:
         bot.send_message(chat_id, "there is nothing we can do...")
+
+
 
 
 
